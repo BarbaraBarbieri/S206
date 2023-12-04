954d081dc22f77b7952191621cbad8b8e50d5047
Bárbara Martins Barbieri <barbaramartinsbarbieri@gmail.com>
2023-12-04 20:06:03 -0300

    feat: add karate test for StarWars API

---
+++ karate-tests/src/test/java/classes/starwars/StarWarsRunner.java
@@ -0,0 +1,11 @@
+package classes.starwars;
+
+import com.intuit.karate.junit5.Karate;
+
+public class StarWarsRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("starwars").relativeTo(getClass());
+    }
+}
